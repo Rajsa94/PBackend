982e9840d8843d2a91ed071b7f0eae2fd391a8c7
Rajsa94 <115164768+Rajsa94@users.noreply.github.com>
2023-01-14 08:06:38 +0530

    first commit hi bro

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ require('dotenv').config()
 const passport = require('passport')
 var session = require('express-session')
 const MongoDbStore = require('connect-mongo')
+const cors = require("cors")
+
+const corsOpts = {
+    origin: 'rsrathore.netlify.app',
+    credentials: true,
+    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
+    allowedHeaders: ['Content-Type'],
+    exposedHeaders: ['Content-Type']
+};
+app.use(cors(corsOpts));
 
 // userlogin funsnality define
 app.use(session({
